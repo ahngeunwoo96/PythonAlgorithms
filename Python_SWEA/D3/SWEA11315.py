902f10f648408746c26452c2d05ecb3df00f0b34
ahngeunwoo96 <960722rjsdn@naver.com>
2021-02-01 23:28:36 +0900

    modified 21.02.01

--- Python_SWEA/D3/SWEA11315.py
+++ Python_SWEA/D3/SWEA11315.py
@@ -10,6 +10,7 @@ for test_case in range(1, int(input()) + 1):
         for j in range(len(row)):
             pan[i][j] = row[j]
 
+    # 가로 확인 
     for i in range(n):
         cnt = 0
         for j in range(n):
@@ -31,25 +32,48 @@ for test_case in range(1, int(input()) + 1):
                 if cnt >= 5:
                     result = 'YES'
                     break
+ 
+    # 오른쪽 아래로 대각선
+    for i in range(n - 4):
+        cnt = 0
+        for j in range(n - i):
+            if pan[j][i+j] == '.':
+                cnt = 0
+            else:
+                cnt += 1
+                if cnt >= 5:
+                    result = 'YES'
+                    break
         
-    cnt = 0
-    for i in range(n):
-        if pan[i][i] == '.':
-            cnt = 0
-        else:
-            cnt += 1
-            if cnt >= 5:
-                result = 'YES'
-                break
+        cnt = 0
+        for j in range(n - i):
+            if pan[i + j][j] == '.':
+                cnt = 0
+            else:
+                cnt += 1
+                if cnt >= 5:
+                    result = 'YES'
+                    break
+        
 
-    cnt = 0
-    for i in range(n):
-        if pan[i][n-1-i] == '.':
-            cnt = 0
-        else:
-            cnt += 1
-            if cnt >= 5:
-                result = 'YES'
-                break
+    # 왼쪽 아래로 대각선
+    for i in range(n - 4):
+        for j in range(n - i):
+            if pan[j][n-1-i-j] == '.':
+                cnt = 0
+            else:
+                cnt += 1
+                if cnt >= 5:
+                    result = 'YES'
+                    break
+
+        for j in range(n - i):
+            if pan[i+j][n-1-j] == '.':
+                cnt = 0
+            else:
+                cnt += 1
+                if cnt >= 5:
+                    result = 'YES'
+                    break            
 
     print('#{} {}'.format(test_case, result))
