29a4603f11f6f9066642dd294d77e991331e8667
ahngeunwoo96 <960722rjsdn@naver.com>
2021-02-12 19:51:50 +0900

    modified 210212

--- Python_bj/1932bj.py
+++ Python_bj/1932bj.py
@@ -8,12 +8,13 @@ for i in range(n):
     triangle.append(list(map(int, input().split())))
     max_sum.append([0] * (i + 1))
 
-for i in range(n-2, -1, -1):
-    for j in range(i):
-        for k in range(2):
-            if max_sum[i][j] < triangle[i][j] + triangle[i+1][j+k]:
-                max_sum[i][j] = triangle[i][j] + triangle[i+1][j+k]
+max_sum[n-1] = triangle[n-1]
 
+for i in range(n-2, -1, -1):
+    for j in range(i+1):
+        for k in range(2):
+            if max_sum[i][j] < triangle[i][j] + max_sum[i+1][j+k]:
+                max_sum[i][j] = triangle[i][j] + max_sum[i+1][j+k]
 
 print(max_sum[0][0])
 
