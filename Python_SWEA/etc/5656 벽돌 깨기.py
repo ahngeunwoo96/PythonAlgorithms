f80c8008a9940328d9093ad16ae8901ffabcd7ad
ahngeunwoo96 <960722rjsdn@naver.com>
2021-03-18 09:12:32 +0900

    ADD SWEA 5656 벽돌 깨기

---
+++ Python_SWEA/etc/5656 벽돌 깨기.py
@@ -0,0 +1,11 @@
+def Game(tot, cnt):
+    for i in range(W):
+        for j in range(H-1, -1, -1):
+            if m[j][i] != 0:
+
+
+for tc in range(1, int(input()) + 1):
+    N, W, H = map(int, input().split())
+    m = [list(map(int, input().split())) for _ in range(H)]
+
+    Game(0, 0)
