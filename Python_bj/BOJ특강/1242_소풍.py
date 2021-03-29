8a91034a03a040492afb31bea06b6f5e56354a57
ahngeunwoo96 <960722rjsdn@naver.com>
2021-03-29 23:28:38 +0900

    UPDATE BOJ 1242

--- Python_bj/BOJ특강/1242_소풍.py
+++ Python_bj/BOJ특강/1242_소풍.py
@@ -1,34 +1,24 @@
-N, K, M = map(int, input().split())
+N, K, M = map(int,input().split())
+result = 0
+tmp_m = M
 
-# 차례대로 숫자를 말하고 K에 해당하는 숫자를 말한 사람이 퇴장당하는 게임
-out_cnt = 0
-num = -1
-M -= 1 # 동호의 위치는 실제 번호보다 1 작으므로
-tot_num = N
-nM = 0
-
-while True:
-    # 매 라운드마다 동호의 위치와 퇴장당하는 위치를 계산
-    num = (num+K)
-    if num < N:
-        out_cnt += 1
-    elif num >= N:
-        num = num % N
-        N -= out_cnt
-        out_cnt = 1
-        if num >= N:
-            num %= N
-            N -= out_cnt
-        M -= nM
-        nM = 0
-
-    if N == 1:
+while result < N:
+    result += 1
+    # 남아있는 수보다 K가 큰 경우 동호가 수가 지워지는 경우
+    if K % (N - result + 1) == tmp_m % (N - result + 1):
         break
 
-    if num < M:
-        nM += 1 # 동호의 위치 변화
-    elif num == M:
+    # 동호의 수가 지워지는 경우
+    if K == tmp_m:
         break
 
-print(tot_num - N + out_cnt)
+    # m의 위치가 K만큼 이동
+    # N, K, M = 5, 2, 3 일때
+    # 1, 2, 3, 4, 5 에서 2가 지워지고 3, 4, 5, 1 이렇게 수의 배열이 변화하는 것으로 생각
+    # 다음은 K 번째인 4가 지워지고 5, 1, 3 이렇게 수의 배열이 됨 -> 3, 5 -> 3
+    else:
+        tmp_m -= K
+        while tmp_m <= 0:
+            tmp_m += (N - result + 1)
 
+print(result)
