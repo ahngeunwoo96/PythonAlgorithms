17fe651361a6ac8caa2476bc2f0c0eff819ffcf1
ahngeunwoo96 <960722rjsdn@naver.com>
2021-04-06 16:06:38 +0900

    UPDATE boj 17406 complete

--- Python_bj/baekjoon_samsung_A형기출/17406_배열_돌리기4.py
+++ Python_bj/baekjoon_samsung_A형기출/17406_배열_돌리기4.py
@@ -1,23 +1,22 @@
 from itertools import permutations
 from copy import deepcopy
+import sys
 
-N, M, K = map(int, input().split())
-m = [list(map(int, input().split())) for _ in range(N)]
-nm = deepcopy(m)
-ans = 5000
+N, M, K = map(int, sys.stdin.readline().split())
+nm = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+ans = 500000
 
 # 회전 연산의 배열
 rotate = []
 for i in range(K):
-    rotate.append(list(map(int, input().split())))
+    rotate.append(list(map(int, sys.stdin.readline().split())))
 
 ran = list(permutations(range(len(rotate)), len(rotate)))
-print(rotate)
 
 for j in range(len(ran)): # 회전 연산의 랜덤 수열의 개수
     order = list(map(int, ran[j]))
+    m = deepcopy(nm)
     for i in range(len(order)):
-        m = deepcopy(nm)
         r, c, s = rotate[order[i]][0], rotate[order[i]][1], rotate[order[i]][2]
         r1, c1, r2, c2 = r-s-1, c-s-1, r+s-1, c+s-1
 
@@ -34,7 +33,7 @@ for j in range(len(ran)): # 회전 연산의 랜덤 수열의 개수
 
             temp2 = temp
             temp = m[r2-k][c1+k]
-            for col in range(c1+k, c2-k): # 왼쪽으로 이동
+            for col in range(c1+k, c2-k-1): # 왼쪽으로 이동
                 m[r2-k][col] = m[r2-k][col+1]
             m[r2-k][c2-k-1] = temp2
 
@@ -50,13 +49,21 @@ for j in range(len(ran)): # 회전 연산의 랜덤 수열의 개수
 
 print(ans)
 '''
-5 6 3
+5 6 2
 1 2 3 2 5 6
 3 8 7 2 1 3
 8 2 3 1 4 5
 3 4 5 1 1 1
 9 3 2 1 4 3
-3 4 2
 4 2 1
+3 4 2 
+'''
+
+'''
+3 3 2
+1 2 3
+4 5 6
+7 8 9
+2 2 1
 2 2 1
 '''
